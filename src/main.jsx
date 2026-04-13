eb91992689805b54f96b722ae9d01d52a3b733b5
itsloraee <laurxee@gmail.com>
2026-04-12 20:45:18 -0400

    Replace App component with SeyrinSite component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import SeyrinSite from "./seyrin_site.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <SeyrinSite />
   </StrictMode>
 );
